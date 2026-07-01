cf1c8ee9b04ee42cf8814baff95fb4b028c15b65
vhemanthreddy462-python <v.hemanthreddy462@gmail.com>
2026-07-01 13:13:45 +0530

    New file.py

---
+++ newfile.py
@@ -0,0 +1,8 @@
+#pyton_caculate_ptofit
+name = "prasad amount gain"
+print(name)
+amount = float(input("one_ton_bannana: "))
+#a bannana fruit sales man
+six_tons_of_bannanas = float(input("number of tons : "))
+amount_gained = six_tons_of_bannanas*amount
+print(amount_gained)
